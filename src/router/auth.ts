0720c74e7b8221d5eb84c42c449ca83b043e3d50
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-15 14:25:03 +0530

    created signin route

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -34,3 +34,43 @@ router.post("/user/signup", async (req: Request, res: Response) => {
     });
   }
 });
+
+router.post("/user/signin", async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await db.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!userExist) {
+      res.status(404).json("User not found");
+      return;
+    }
+
+    const isPasswordValid = bcrypt.compare(password, userExist.password);
+
+    if (!isPasswordValid) {
+      res.status(403).json({
+        msg: "wrong Credentials",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      { id: userExist.id },
+      process.env.JWT_SECRET as string
+    );
+    res.json({
+      token,
+      userId: userExist.id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: "Something went wrong"
+    })
+  }
+});
+
+export { router as authRouter };
