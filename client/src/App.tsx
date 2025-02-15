bd9ae9b2b0f7920f73922e107d387acbd2bd93bf
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-16 01:34:45 +0530

    added react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import KanbanBoard from "./components/KanbanBoard"
-import { KanbanProvider } from "./context/KanbanContext"
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <KanbanProvider>
-      <KanbanBoard />
-    </KanbanProvider>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>       
+    </>
   )
 }
 
