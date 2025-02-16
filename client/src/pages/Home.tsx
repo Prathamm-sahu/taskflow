4ef1824a0de80cec5cfd67360304126f53403e55
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-17 03:09:23 +0530

    Added fetch colum logic in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import KanbanBoard from "../components/KanbanBoard";
 import Navbar from "../components/Navbar";
 import { useKanban } from "../context/KanbanContext";
+import { Column } from "../types";
+import axios from "axios";
+import { BACKEND_URL } from "../config/url";
 
 function Home() {
-  const { state } = useKanban();
+  const { state, dispatch } = useKanban();
   const [searchQuery, setSearchQuery] = useState("");
 
   const filteredColumns = state.columns.map((column) => ({
@@ -13,6 +16,37 @@ function Home() {
       task.title.toLowerCase().includes(searchQuery.toLowerCase())
     ),
   }));
+
+  const fetchData = async () => {
+    try {
+      const { data }: { data: Column[] } = await axios.get(
+        `${BACKEND_URL}/task/columnData`,
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+
+      data.map((col) => {
+        dispatch({
+          type: "ADD_COLUMN",
+          column: {
+            id: col.id,
+            title: col.title,
+            tasks: col.tasks,
+          },
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
   return (
     <>
       <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
