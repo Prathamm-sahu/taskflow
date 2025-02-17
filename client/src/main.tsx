3fb8d384321b4e754110f58ffb3d1b61bd355d90
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-17 09:08:11 +0530

    added strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,13 @@ import "./styles/index.css";
 import App from "./App.tsx";
 import { KanbanProvider } from "./context/KanbanContext.tsx";
 import { Toaster } from "sonner";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
-
+  <StrictMode>
     <KanbanProvider>
       <Toaster />
       <App />
     </KanbanProvider>
-
+  </StrictMode>
 );
