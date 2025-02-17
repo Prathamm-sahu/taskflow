9cda922d65012ac9b92e06d4dd3c743d4f7df73e
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-17 08:09:58 +0530

    removed unused imports

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import { ChangeEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { BACKEND_URL } from "../config/url";
