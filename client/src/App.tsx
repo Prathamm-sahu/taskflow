06b42b45ce454eb98def433d257a5232f3b38dc1
Pratham Sahu <prathamsahu051@gmail.com>
2025-02-17 08:37:02 +0530

    added page not found

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SignUp from "./pages/SignUp"
 import SignIn from "./pages/SignIn"
 import Home from "./pages/Home"
+import PageNotFound from "./components/PageNotFound"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </Router>       
     </>
